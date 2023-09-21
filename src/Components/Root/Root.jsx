0517f77172db579e4a30eaa71da4caf605ca9a79
rifat3790 <mdrifayethossen@gmail.com>
2023-09-21 21:53:09 +0600

    added footer

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
 
 const Root = () => {
     return (
         <div>
             <h1>Hello</h1>
+            <div>
             <Outlet></Outlet>
+            <Footer></Footer>
+            </div>
         </div>
     );
 };
